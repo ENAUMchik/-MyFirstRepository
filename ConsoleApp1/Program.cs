9556a82c60a5427ce6b3cd75fb2e01e10afe61a2
ENAUMchik <elenanaumova1406@yandex.ru>
2024-03-08 14:02:53 +0300

    Практикум модуль 4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,32 +1,56 @@
-﻿class MainClass
+﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+class MainClass
 {
+
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-        var color = Console.ReadLine();
-
-        if (color == "red")
+        int k = 0;
+        while (k < 3)
         {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is red!");
+            (string name, string lastname, string login, bool havePet, double age, string[] favcolors) users;
+            Console.WriteLine("Введите имя");
+            users.name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            users.lastname = Console.ReadLine();
+            Console.WriteLine("Введите логин");
+            string login = Console.ReadLine();
+            Console.WriteLine("Количество символов в логине:");
+            string[] loginLength = new string[login.Length];
+            Console.WriteLine(login.Length);
+            Console.WriteLine("Есть ли у Вас питомец?Да или нет?");
+            var result = Console.ReadLine();
+            if (result == "Да")
+            {
+                users.havePet = true;
+            }
+            else
+            {
+                users.havePet = false;
+            }
+            Console.WriteLine("Укажите Ваш возраст");
+            users.age = double.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите три Ваших любимых цвета");
+            string[] favcolors = new string[3];
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = Console.ReadLine();
+            }
+            k++;
         }
-
-        else if (color == "green")
-        {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is green!");
-        }
-        else
-        {
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is cyan!");
-        }
+        
     }
+
+
+    
+
+
 }
+
+
+
+
+
+
